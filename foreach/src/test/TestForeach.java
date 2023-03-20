7cc33764bb087c36416b86b5daf4e3292ef2723e
iamangelmp <alexis.mplt@outlook.es>
2023-03-20 00:17:29 -0600

    using foreach like and alternative to for

---
+++ foreach/src/test/TestForeach.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+
+/**
+ *
+ * @author Alexis
+ */
+public class TestForeach {
+    public static void main(String[] args) {
+        String[] nombres = {"angel", "juan", "fernanda", "sam"};
+        
+        for(String nombre: nombres){
+            System.out.println(nombre);
+        }
+    }
+    
+}
