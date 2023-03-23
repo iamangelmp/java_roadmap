3589c47dd04aa72331e2bd3d1cfc6ccc34aa9d77
iamangelmp <alexis.mplt@outlook.es>
2023-03-23 13:38:32 -0600

    apply polymorfism using instances from Interfaces

--- Interfaces/src/test/TestInterfaces.java
+++ Interfaces/src/test/TestInterfaces.java
@@ -4,12 +4,25 @@
  */
 package test;
 
+import accesoDatos.*;
+
 /**
  *
  * @author Alexis
  */
 public class TestInterfaces {
+
     public static void main(String[] args) {
-        System.out.println("hello");
+        IAccesoDatos conexion = new ImplementacionMySQL();
+
+        imprimir(conexion);
+
+        conexion = new ImplementacionOracle();
+        conexion.actualizar();
+
+    }
+
+    public static void imprimir(IAccesoDatos datos) {
+        datos.listar();
     }
 }
