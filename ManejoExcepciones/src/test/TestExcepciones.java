0eb70fa88c01c3f40b6104bd9b3c242e8982bedf
iamangelmp <alexis.mplt@outlook.es>
2023-03-25 23:55:22 -0600

    test exception try catch

--- ManejoExcepciones/src/test/TestExcepciones.java
+++ ManejoExcepciones/src/test/TestExcepciones.java
@@ -12,7 +12,6 @@ public class TestExcepciones {
     public static void main(String[] args) {
         int resultado = 0;
         
-        
         try{
             resultado = 10/0;
             System.out.println("resultado = " + resultado);
