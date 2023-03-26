a75a7ffc250948b126ec4770240bea1f0ab95feb
Angel Marquez <alexis.mplt@outlook.es>
2023-03-26 17:17:57 -0600

    using Set and HashSet

--- APIColeccion/src/test/TestcColecciones.java
+++ APIColeccion/src/test/TestcColecciones.java
@@ -13,21 +13,38 @@ import java.util.*;
 public class TestcColecciones {
 
 	public static void main(String[] args) {
+		
+		//List and ArrayList
 		List semana = new ArrayList();
 		semana.add("Lunes");
 		semana.add("Martes");
 		semana.add("Miercoles");
 		semana.add("Jueves");
 		semana.add("Viernes");
+		
+		//imprimir(semana);
+		
+		//Set and hashSet
+		Set meses = new HashSet();
+		meses.add("enero");
+		meses.add("febrero");
+		meses.add("marzo");
+		meses.add("abril");
+		meses.add("mayo");
+		
+		imprimir(meses);
 
+	}
+
+	public static void imprimir(Collection lista) {
 		//using a native foreach
-		for (Object dia : semana) {
-			System.out.println("dia: " + dia);
-		}
+//		for (Object dia : semana) {
+//			System.out.println("dia: " + dia);
+//		}
 
 		//using an lambda expresion (arrow function)
-		semana.forEach(dia -> {
-			System.out.println("dia: " + dia);
+		lista.forEach(elemento -> {
+			System.out.println("dia: " + elemento);
 		});
 	}
 }
