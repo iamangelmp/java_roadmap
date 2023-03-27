826dcc37eb5271d81c9001461dc4c562655b65eb
Angel Marquez <alexis.mplt@outlook.es>
2023-03-27 15:35:19 -0600

    creating class to create a document

---
+++ ManejoArchivos/src/ManejoArchivo/ManejoArchivo.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ManejoArchivo;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author Ebit PC 2
+ */
+public class ManejoArchivo {
+    public static void crearArchivo(String nombreArchivo) throws FileNotFoundException{
+        File archivo = new File(nombreArchivo);
+        try{
+            PrintWriter salida = new PrintWriter(archivo);
+            salida.close();
+            System.out.println("Se ah creado el archivo");
+        }catch(FileNotFoundException e){
+            System.out.println("Ocurrio un error: ");
+            e.printStackTrace(System.out);
+            System.out.println(e.getMessage());
+        }
+    }
+}
