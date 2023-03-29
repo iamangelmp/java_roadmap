d72f78ab374a3d9c19e09ca060e7b10b8b76815a
Angel Marquez <alexis.mplt@outlook.es>
2023-03-28 23:33:33 -0600

    creating a method to add and override content in a file

--- ManejoArchivos/src/ManejoArchivo/ManejoArchivo.java
+++ ManejoArchivos/src/ManejoArchivo/ManejoArchivo.java
@@ -6,37 +6,60 @@ package ManejoArchivo;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Ebit PC 2
  */
 public class ManejoArchivo {
-    public static void crearArchivo(String nombreArchivo) {
-        File archivo = new File(nombreArchivo);
-        try{
-            PrintWriter salida = new PrintWriter(archivo);
-            salida.close();
-            System.out.println("Se ha creado el archivo");
-        }catch(FileNotFoundException e){
-            System.out.println("Ocurrio un error: ");
-            e.printStackTrace(System.out);
-            System.out.println(e.getMessage());
-        }
-    }
-    
-    public static void escribirArchivo(String nombreArchivo, String contenido){
-        File archivo = new File(nombreArchivo);
-        try{
-            PrintWriter salida = new PrintWriter(archivo);
-            salida.println(contenido);
-            salida.close();
-            System.out.println("Se ha escrito el cuerpo del archivo");
-        }catch(FileNotFoundException e){
-            System.out.println("Ocurrio un error: ");
-            e.printStackTrace(System.out);
-            System.out.println(e.getMessage());
-        }
-    }
+
+	public static void crearArchivo(String nombreArchivo) {
+		File archivo = new File(nombreArchivo);
+		try {
+			PrintWriter salida = new PrintWriter(archivo);
+			salida.close();
+			System.out.println("Se ha creado el archivo");
+		} catch (FileNotFoundException e) {
+			System.out.println("Ocurrio un error: ");
+			e.printStackTrace(System.out);
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public static void escribirArchivo(String nombreArchivo, String contenido) {
+		File archivo = new File(nombreArchivo);
+		try {
+			PrintWriter salida = new PrintWriter(archivo);
+			salida.println(contenido);
+			salida.close();
+			System.out.println("Se ha sobreescrito el cuerpo del archivo");
+		} catch (FileNotFoundException e) {
+			System.out.println("Ocurrio un error: ");
+			e.printStackTrace(System.out);
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public static void anexarInfo(String nombreArchivo, String contenido) {
+		File archivo = new File(nombreArchivo);
+		try {
+			PrintWriter salida = new PrintWriter(new FileWriter(archivo, true));
+			salida.println(contenido);
+			salida.close();
+			System.out.println("Se ha escrito el cuerpo del archivo");
+		} catch (FileNotFoundException ex) {
+			System.out.println("Ocurrio un error: ");
+			ex.printStackTrace(System.out);
+			System.out.println(ex.getMessage());
+		} catch (IOException ex) {
+			System.out.println("Ocurrio un error: ");
+			ex.printStackTrace(System.out);
+			System.out.println(ex.getMessage());
+		} 
+	}
 }
--- ManejoArchivos/src/test/TestManejoArchivos.java
+++ ManejoArchivos/src/test/TestManejoArchivos.java
@@ -4,23 +4,30 @@
  */
 package test;
 
+import static ManejoArchivo.ManejoArchivo.anexarInfo;
 import static ManejoArchivo.ManejoArchivo.escribirArchivo;
 import static ManejoArchivo.ManejoArchivo.crearArchivo;
+
 /**
  *
  * @author Ebit PC 2
  */
 public class TestManejoArchivos {
-    public static void main(String[] args) {
-        var nombreDoc = "prueba.txt";
-	
-	//crea el archivo
-        //crearArchivo(nombreDoc);
-	
-	
-	//agrega contenido al archivo
-	escribirArchivo(nombreDoc, "Este es el contenido");
-	
-	
-    }
+
+	public static void main(String[] args) {
+		var nombreDoc = "prueba.txt";
+
+		//crea el archivo
+		//crearArchivo(nombreDoc);
+		
+		//sobreescribe contenido al archivo
+		//escribirArchivo(nombreDoc, "Este es el contenido");
+		
+		//escribe contenido del archivo
+		anexarInfo(nombreDoc, "Este es la línea 1");
+		anexarInfo(nombreDoc, "Esta es la línea 2");
+		
+		
+
+	}
 }
