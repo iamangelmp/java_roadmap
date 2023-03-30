a94edbe8e7f13bde6704978a8adeda6713a1e8b9
Angel Marquez <alexis.mplt@outlook.es>
2023-03-29 21:43:40 -0600

    practice using interfaces

---
+++ CatalogoPeliculas/src/domain/Pelicula.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author Alexis
+ */
+public class Pelicula {
+
+	private String nombre;
+
+	public Pelicula() {
+	}
+
+	public Pelicula(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Nombre Pelicula: " + nombre;
+	}
+}
