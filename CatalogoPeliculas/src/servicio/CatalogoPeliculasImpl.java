358b221fc63f84ad71e3d9adda05f0dd02bf906e
Angel Marquez <alexis.mplt@outlook.es>
2023-04-06 22:29:30 -0600

    implementation and interface class

---
+++ CatalogoPeliculas/src/servicio/CatalogoPeliculasImpl.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package servicio;
+
+import accesodatos.*;
+
+/**
+ *
+ * @author Alexis
+ */
+public class CatalogoPeliculasImpl implements IPeliculas {
+	
+	private final IAccesoDatos datos;
+	
+	public CatalogoPeliculasImpl()[{}
+		this.datos = new IAccesoDatos;
+	}
+
+	@Override
+	public void agregarPeliculas(String nombrePelicula) {
+		throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+	}
+
+	@Override
+	public void listarPeliculas() {
+		throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+	}
+
+	@Override
+	public void buscarPelicula(String buscar) {
+		throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+	}
+
+	@Override
+	public void iniciarPeliculas() {
+		throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+	}
+	
+}
