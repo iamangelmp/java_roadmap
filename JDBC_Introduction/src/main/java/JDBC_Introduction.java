e49c180c799f3a2e17767278d384110e37ea0a17
Angel Marquez <alexis.mplt@outlook.es>
2023-04-08 11:47:11 -0600

    dependencies for JDBC

---
+++ JDBC_Introduction/src/main/java/JDBC_Introduction.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Alexis
+ */
+public class JDBC_Introduction {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
